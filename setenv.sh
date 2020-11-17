490e9f137a219d288b96977595ec0a7ae9e2f7ac
Selin Kanatli <selin@tropos.io>
2020-11-17 10:04:05 +0100

    First push

--- setenv.sh
+++
@@ -1,5 +0,0 @@
-export GIT_RSA=`cat ~/.ssh/id_rsa.pub`
-export DB_RSA=`cat ~/.ssh/id_rsa.pub`
-export REPO='git@bitbucket.org:tropos/tf-snowflake-privatelink.git'
-echo awk -F/ '{print $3}' <<<$REPO
-
